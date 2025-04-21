eade4e043c3410d6b770c0214ca2073a6e36a00b
Anderson Marzola <ajmarzola@gmail.com>
2025-04-21 13:51:00 -0300

    models

---
+++ src/FCG.Application/Model/CompraModel.cs
@@ -0,0 +1,27 @@
+﻿using FCG.Application.DTOs;
+using FCG.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCG.Application.Model
+{
+    public class CompraModel
+    {
+        public Guid Id { get; set; }
+
+        public Guid UsuarioId { get; set; }
+
+        public UsuarioRegistroModel? Usuario { get; set; }
+
+        public Guid JogoId { get; set; }
+
+        public Jogo? Jogo { get; set; }
+
+        public DateTime DataCompra { get; set; }
+
+        public decimal ValorPago { get; set; }
+    }
+}

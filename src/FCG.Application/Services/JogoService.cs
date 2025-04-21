41f60194307ee6335ec12af539fe336779298fbd
Anderson Marzola <ajmarzola@gmail.com>
2025-04-21 09:18:06 -0300

    jogo service

--- src/FCG.Application/Services/JogoService.cs
+++ src/FCG.Application/Services/JogoService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FCG.Application.Services;
+using FCG.Domain.Entities;
+using FCG.Domain.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,38 @@ using System.Threading.Tasks;
 
 namespace FCG.Domain.Services
 {
-    internal class JogoService
+    internal class JogoService : IJogoService
     {
+        private readonly IJogoRepository _jogoRepository;
 
+        public JogoService (IJogoRepository jogoRepository)
+        {
+            _jogoRepository = jogoRepository;
+        }
+
+        public Task<bool> AlterarJogo(Jogo model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Jogo> BucarJogo(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeletarJogo(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Jogo>> ListaJogo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SavarJogo(Jogo model)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

d3045fee290415418e2737e7c421c6688cef8601
Rafael Nicoletti <rafaelnicoletti@hotmail.com>
2025-04-23 20:26:25 -0300

    mesclar

---
+++ src/FCG.Application/Services/IUsuarioService.cs
@@ -0,0 +1,20 @@
+﻿using FCG.Application.DTOs;
+using FCG.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCG.Application.Services
+{
+    public interface IUsuarioService
+    {
+        Task<Usuario> BuscarUsuarioIdAsync(Guid id);
+        Task<Usuario> BuscarUsuarioEmailAsync(string email);
+        Task<IEnumerable<Usuario>> ListaUsuariosAsync();
+        Task<bool> SalvarUsuarioAsync(UsuarioRegistroModel usuario);
+        Task<bool> AlterarAsync(UsuarioRegistroModel usuario);
+        Task<bool> DeletarUsuarioAsync(Guid id);
+    }
+}

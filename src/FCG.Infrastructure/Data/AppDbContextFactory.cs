463bfb560e3d06757af04f5c68895547284a077f
Anderson Marzola <ajmarzola@gmail.com>
2025-07-28 23:13:47 -0300

    new relic e docker e banco na nuvem

---
+++ src/FCG.Infrastructure/Data/AppDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace FCG.Infrastructure.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer("Server=grupo49-fcg.database.windows.net;Database=FCGDb;User ID=usrfcgapp;Password=Grupo49FI@P;Encrypt=True;TrustServerCertificate=True");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}

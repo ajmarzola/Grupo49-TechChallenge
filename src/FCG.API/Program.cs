5ef0b435bd8281d682caceeca736eb5ecc577b4f
Anderson Marzola <ajmarzola@gmail.com>
2025-08-02 16:44:26 -0300

    outro ajuste na porta - forçar porta 80

--- src/FCG.API/Program.cs
+++ src/FCG.API/Program.cs
@@ -19,7 +19,8 @@ using System.Collections.Generic;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-//builder.WebHost.UseUrls("http://0.0.0.0:8080");
+builder.WebHost.UseUrls("http://0.0.0.0:80");
+
 // 1. Conexão com LocalDB
 //builder.Services.AddPooledDbContextFactory<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 

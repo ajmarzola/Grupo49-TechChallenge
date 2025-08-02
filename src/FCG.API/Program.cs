6f538d11495e81ea0cc05a5a573508e1dbd9f12f
Anderson Marzola <ajmarzola@gmail.com>
2025-08-02 17:15:47 -0300

    não é possivel

--- src/FCG.API/Program.cs
+++ src/FCG.API/Program.cs
@@ -19,7 +19,7 @@ using System.Collections.Generic;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.UseUrls("http://0.0.0.0:80");
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
 
 // 1. Conexão com LocalDB
 //builder.Services.AddPooledDbContextFactory<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
